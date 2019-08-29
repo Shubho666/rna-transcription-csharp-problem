3998b12ced14d2432f2c52145a590bd238cd5505
shubhajyoti.paul <shubhajyoti.paul@cgi.com>
2019-08-29 15:12:29 +0530

    ver 1.0.1

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RNATranscriptionModule
 {
@@ -6,7 +8,20 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+            
+            var subs = new Dictionary<string, string> { {"G","W"},{"C","X"},{"T","Y"},{"A","Z"} };
+            var subs1 = new Dictionary<string, string> { {"W","C"},{"X","G"},{"Y","A"},{"Z","U"} };
+        string va = nucleotide;
+        va = subs.Aggregate(va, (result, s) => result.Replace(s.Key, s.Value));
+        va=subs1.Aggregate(va, (result, s) => result.Replace(s.Key, s.Value));
+        return va;
+        }
+
+
+        public static void Main(string[] args) 
+        {
+        //    ToRna("ACSKKFKS");
         }
     }
 }
